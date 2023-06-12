66bcdd349bb6bfff949454bca41a4c81351d2dfe
Nikita Makarov <quiz.beat@gmail.com>
2023-06-12 11:14:01 +0300

    Initial Commit

---
+++ ComposableArchitecture-AuthCase/ComposableArchitecture_AuthCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableArchitecture_AuthCaseApp.swift
+//  ComposableArchitecture-AuthCase
+//
+//  Created by Nikita Makarov on 12.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ComposableArchitecture_AuthCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
